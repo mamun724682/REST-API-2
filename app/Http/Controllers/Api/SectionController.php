f5b111818fb7197de3d034b5adbb9405e1b6a3c5
Abdullah Al Mamun <alexmamun085@gmail.com>
2020-09-21 19:49:22 +0600

    Section CRUD implemented

---
+++ app/Http/Controllers/Api/SectionController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Model\Section;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sections = Section::all();
+        return response()->json($sections);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'class_id' => 'required|max:25',
+            'section_name' => 'required|unique:sections|max:25',
+        ]);
+
+        $section = Section::create($request->all());
+
+        return response('Created Successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $section = Section::findOrFail($id);
+        return response()->json($section);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'class_id' => 'max:25',
+            'section_name' => 'unique:sections|max:25',
+        ]);
+
+        $section = Section::findOrFail($id);
+        $section->update($request->all());
+
+        return response('Updated Successfully!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Section::findOrFail($id)->delete();
+
+        return response('Deleted Successfully!');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/class', 'Api\SclassController');
 Route::apiResource('/subject', 'Api\SubjectController');
+Route::apiResource('/section', 'Api\SectionController');
