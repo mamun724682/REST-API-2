e904331093cadcc20fc92641eb9d4636c10ad3b7
Abdullah Al Mamun <alexmamun085@gmail.com>
2020-09-22 10:57:22 +0600

    JWT auth implemented and register functionality added

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,19 @@ Route::apiResource('/class', 'Api\SclassController');
 Route::apiResource('/subject', 'Api\SubjectController');
 Route::apiResource('/section', 'Api\SectionController');
 Route::apiResource('/student', 'Api\StudentController');
+
+Route::group([
+
+    // 'middleware' => 'api',
+    // 'namespace' => 'App\Http\Controllers',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('register', 'AuthController@register');
+
+});
